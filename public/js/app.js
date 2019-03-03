7fe7e9a133ea932b879d427d29b19d811aa2626e
Melvin Hernandez <melher39@gmail.com>
2019-03-02 22:14:32 -0600

    delete comments function is working

--- public/js/app.js
+++ public/js/app.js
@@ -1,41 +1,68 @@
 // this will be our js file for front-end app logic
 // wait for the document to load and be ready
-$(document).ready(function() {
+$(document).ready(function () {
     // this button should load the scraped articles
-    $("#scrape-button").on("click", function(event){
+    $("#scrape-button").on("click", function (event) {
         // 
         // event.preventDefault();
         // send the GET request to scrape the thrasher site
         $.ajax("/scrape", {
             type: "GET"
             // then send another GET request to gather all the articles retrieved
-    }).then(function(){
-        $.ajax("/all", {
-            type: "GET"
-        }).then(function(){
-            location.href = "/all";
+        }).then(function () {
+            $.ajax("/all", {
+                type: "GET"
+            }).then(function () {
+                location.href = "/all";
+            });
         });
     });
-    });
 
-    $(".comments-button").on("click", function(){
-        alert("it works");
+    $(".comments-button").on("click", function () {
+        // alert("it works");
+        // empty the div before adding new comments so comments are not piled up infinitely
+        $("#comment-body").empty();
+
+
         let articleId = $(this).data("id");
-        $.get("/articles/"+ articleId, function(result){
+        $.get("/articles/" + articleId, function (result) {
             console.log("this is the article with the comments: " + result);
 
-            for (let i=0; i <result.comment.length; i++){
-                let comment = result.comment[i].body;
-                console.log(comment);
-                $("#comment-body").append(comment);
+            for (let i = 0; i < result.comment.length; i++) {
+                let articleComment = result.comment[i].body;
+                console.log(articleComment);
+
+
+                let deleteButton = $("<button>");
+                deleteButton.addClass("btn right delete-button modal-close");
+                // deleteButton.addClass("right");
+                deleteButton.text("X");
+                deleteButton.attr("data-id", result.comment[i]._id);
+                $("#comment-body").append("<div>", articleComment, deleteButton, "</div>");
+
 
             }
-            // jquery for modal pop-up    
-            $("#comment-modal").modal("open");
-           
         });
 
+        $("#comment-modal").modal("open");
+
+
     });
+
+    $(document).on("click", ".delete-button", function () {
+        alert("delete button works");
+        let commentId = $(this).data("id");
+        $.ajax("/comments/" + commentId, {
+            type: "DELETE"
+        }).then(
+            function () {
+                $("#comment.modal").modal("close");
+            });
+    });
+
+    // $(".modal-close").on("click", function(){
+
+    // })
 
     $(".modal").modal();
 
